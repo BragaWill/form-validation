88f532706d3560332bf87cbb8d5bc55866773371
bragawill <marotzk@gmail.com>
2021-09-09 00:12:56 -0300

    form validation logic

---
+++ README.md
@@ -0,0 +1,26 @@
+Form validation example
+====================================
+An example validate forms using back end logic
+
+# Running locally
+With Node installed, install the dependencies with:
+```sh
+$ npm i
+```
+
+and finally run:
+```sh
+nodemon index.js
+```
+or run: 
+```sh
+npx nodemon index.js
+```
+
+listening: 
+```
+http://localhost:5678/
+```
+
+# License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- index.js
+++ index.js
@@ -1,21 +1,76 @@
 const express = require('express') 
 const flash = require('express-flash')
 const session = require('express-session')
+const cookieParser = require('cookie-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+app.use(cookieParser('asdfasdf'))
 app.use(session({
   secret: 'keybord cat',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure:true }
+  cookie: { maxAge: 60000 }
 }))
 app.use(flash())
 
 app.get('/', (req, res) => {
-  res.send('Online!')
+  var emailError = req.flash('emailError')
+  var pointError = req.flash('pointError')
+  var nameError = req.flash('nameError')
+
+  var email = req.flash('email')
+  var name = req.flash('name')
+  var points = req.flash('points')
+
+  emailError = (emailError === undefined || emailError.length === 0) ? undefined: emailError
+  pointError = (pointError === undefined || pointError.length === 0) ? undefined: pointError
+  nameError = (nameError === undefined || nameError.length === 0) ? undefined: nameError
+
+  email = (email === undefined || email.length === 0) ? '': email
+  name = (name === undefined || name.length === 0) ? '': name
+  points = (name === undefined || isNaN(points)) ? undefined: points
+  
+  
+  res.render('index',{emailError, pointError, nameError, email, name, points})
+})
+
+app.post('/form', (req, res) => {
+  var {email, name, points} = req.body
+  var emailError, pointError, nameError
+
+  if (email === undefined || email === ''){
+    emailError = 'O email deve ser definido.'
+  }
+
+  if (name === undefined || name === ''){
+    nameError = 'O nome deve ser definido.'
+  }
+
+  if (points === undefined || points < 0 || points > 100){
+    pointError = 'Os pontos devem ser definidos entre 0 e 100.'
+  }
+
+  if (name.length < 2 || name.length > 30){
+    nameError = 'O nome deve ter entre dois e trinta caracteres.'
+  }
+
+  if (emailError !== undefined || pointError !== undefined || nameError !== undefined) {
+    req.flash('emailError', emailError)
+    req.flash('pointError', pointError)
+    req.flash('nameError', nameError)
+
+    req.flash('email', email)
+    req.flash('name', name)
+    req.flash('points', points)
+
+    res.redirect('/')
+  } else {
+    res.send('congratulation')
+  }
 })
 
 
